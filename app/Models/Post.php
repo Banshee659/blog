e34968e3ed5a8c4cabf25487a79477725e2b7a99
Victor Dau <victor@revolutionise.com.au>
2021-08-21 21:23:16 +1000

    cache done

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -32,14 +32,16 @@ class Post
             return static::all()->firstWhere( 'slug', $slug );
     }
 
-    public static function all() {
+    public static function all()
+    {
 
+      return cache()->rememberForever('posts.all', function ()
+      {
         return collect(File::files(resource_path("posts/")))
           ->map(/**
            * @param $file
            * @return \Spatie\YamlFrontMatter\Document
            */ fn($file) => \Spatie\YamlFrontMatter\YamlFrontMatter::parseFile($file))
-
           ->map(/**
            * @param $document
            * @return Post
@@ -49,7 +51,8 @@ class Post
             $document->date,
             $document->body(),
             $document->slug
-          ));
+          ))
+          ->sortByDesc('date');
+      });
     }
-
 }
