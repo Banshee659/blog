3a1040b8f3b5bb0f5c060619c3c6532e91cf28e3
Victor Dau <victor@revolutionise.com.au>
2021-08-22 09:45:28 +1000

    blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,17 +3,17 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-<?php foreach ($posts as $post) : ?>
-<article>
+@foreach ($posts as $post)
+<article class="{{$loop->even ? 'fool' : 'mb-6'}}">
     <h1>
-        <a href="/posts/<?= $post->slug ;?>">
-          <?= $post->title; ?>
+        <a href="/posts/{{$post->slug}}">
+          {{$post->title}}
         </a>
     </h1>
-    <div><?= $post->excerpt; ?></div>
+    <div>{{$post->excerpt}}  </div>
 
 </article>
 
-<?php endforeach;?>
+@endforeach
 </body>
 
