878f42073c6eaa6dc8c8bf4734b4116d1be02a28
Victor Dau <18041104+Banshee659@users.noreply.github.com>
2021-08-29 00:32:39 +1000

    less repetition

--- storage/framework/views/84804375c72cddce455ef88d17ae03bb0603959e.php
+++ storage/framework/views/84804375c72cddce455ef88d17ae03bb0603959e.php
@@ -8,11 +8,11 @@
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
-<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.post-featured-card','data' => []]); ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.post-featured-card','data' => ['post' => $posts[0]]]); ?>
 <?php $component->withName('post-featured-card'); ?>
 <?php if ($component->shouldRender()): ?>
 <?php $__env->startComponent($component->resolveView(), $component->data()); ?>
-<?php $component->withAttributes([]); ?> <?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component->withAttributes(['post' => \Illuminate\View\Compilers\BladeCompiler::sanitizeComponentAttribute($posts[0])]); ?> <?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
 <?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
 <?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
 <?php endif; ?>
